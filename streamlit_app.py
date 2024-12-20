f96185d1a3408f70c6607bf7ff8e9a9ab575fa0c
maxptrnk <maxime.ptrnk@gmail.com>
2024-12-20 11:03:29 -0500

    modifs

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,74 +3,56 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import time
 
 # Set the title of the app
 st.title("Hockey Game Dashboard")
-
-# st.write("Welcome to this simple Streamlit app!")
-
-import time
-
-
-
-    
-
-
-
-
-# Store the initial value of widgets in session state
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = False
-
-
-# ---------------------------
 
 
 with st.sidebar:
     
     st.header("Model Selection")
-
     st.divider()
-    
-    input_workspace = st.text_input("Workspace", "...")
-    input_model = st.text_input("Model", "...")
-    input_version = st.text_input("Version", "...")
 
+#     add_selectbox = st.sidebar.selectbox(
+#     "How would you like to be contacted?",
+#     ("Email", "Home phone", "Mobile phone")
+# )
+
+
+    input_workspace = st.sidebar.selectbox(
+    "Workspace?",
+    ("..."))
+
+    input_model = st.sidebar.selectbox(
+    "Model",
+    ("LogisticReg_Distance_Angle", "..."))
+
+    input_version = st.sidebar.selectbox(
+    "Version",
+    ("lastest", "..."))
+
+    # input_workspace = st.text_input("Workspace", "...")
+    # input_model = st.text_input("Model", "...")
+    # input_version = st.text_input("lastest", "...")
 
     if st.button("Download model", key="download_model"):
             with st.spinner("Loading model from CometML..."):
                 time.sleep(1)
-            st.success("Done!")
+            st.success("Model Loaded!")
 
-            st.write("TODO : checks ")
+
+            # => Brandon : download_registry_model(workspace, model, version)
+            # ? does it return 
 
         
     st.divider()
 
 
-
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
-
 # ---------------------------
-
 
     
 st.divider()
-
-
 col1, col2 = st.columns(2)
 
 with col1:
@@ -82,10 +64,14 @@ with col2:
     input_workspace = st.text_input("Game ID", placeholder="eg: 2021020329",)
 
     if st.button("Ping game"):
-        st.write("TODO : checks ")
+        st.write("TODO : connect with game_client.ping_game ")
 
-
-
+        # X, idx, ... = game_client.ping_game(game_id, idx, ...)
+        # r = requests.post(
+        #     "http://127.0.0.1:<PORT>/predict", 
+        #     json=json.loads(X.to_json())
+        # )
+        # print(r.json())
 
     
 st.divider()
@@ -97,119 +83,32 @@ st.divider()
 
 
 
-
+st.header("GameID HomeTeam vs AwayTeam")
 
 container = st.container(border=True)
-container.write("Home and away team names")
+
 container.write("Period, Time left in the period, Current score")
+
+col1, col2 = container.columns(2)
+
 container.write("Sum of expected goals (xG) for the entire game so far for both teams (obtained from the game client)")
 
+HomeTeam = col1.container(height=120)
+HomeTeam.title("HomeTeam")
 
-
-
+AwayTeam = col2.container(height=120)
+AwayTeam.title("AwayTeam")
 
 
 # =============================================================================
 
 
+st.header("Input Data & Predictions")
+
+container = st.container(border=True)
+container.write("add data frame here")
 
 
 
-
-
-
-st.divider()
-
-
-    
-col1, col2 = st.columns(2)
-
-
-with col1:
-
-
-    st.checkbox("Disable text input widget", key="disabled")
-    st.radio(
-        "Set text input label visibility 👉",
-        key="visibility",
-        options=["visible", "hidden", "collapsed"],
-    )
-    st.text_input(
-        "Placeholder for the other text input widget",
-        "This is a placeholder",
-        key="placeholder",
-    )
-
-with col2:
-    
-
-
-    text_input = st.text_input(
-        "Enter some text 👇",
-        label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled,
-        placeholder=st.session_state.placeholder,
-    )
-
-    if text_input:
-        st.write("You entered: ", text_input)
-
-
-
-
-
-
-# Input Section
-name = st.text_input("What's your name?")
-if name:
-    st.write(f"Hello, {name}!")
-
-# Button to generate random data
-if st.button("Generate Random Data"):
-    # Create a DataFrame with random data
-    data = pd.DataFrame({
-        "X": np.arange(1, 11),
-        "Y": np.random.randint(1, 100, size=10)
-    })
-
-    st.write("Here is your random data:")
-    st.dataframe(data)
-
-    # Plot the data
-    st.write("Here's a chart of your random data:")
-    fig, ax = plt.subplots()
-    ax.plot(data["X"], data["Y"], marker="o")
-    ax.set_title("Random Data Chart")
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    st.pyplot(fig)
-
-
-
-with st.expander("See explanation"):
-    st.write("""
-        The chart above shows some random data.
-        The data is random because I said it was.
-    """)
-
-
-# with st.sidebar:
-#     with st.echo():
-#         st.write("This code will be printed to the sidebar.")
-
-#     with st.spinner("Loading..."):
-#         time.sleep(5)
-#     st.success("Done!")
-
-
-
-# ---------------------------------------------------------------
-
-# X, idx, ... = game_client.ping_game(game_id, idx, ...)
-# r = requests.post(
-# 	"http://127.0.0.1:<PORT>/predict", 
-# 	json=json.loads(X.to_json())
-# )
-# print(r.json())
 
 # # use with docker
